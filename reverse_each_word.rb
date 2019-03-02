5475373acd3f4045135122371aada8a141b9ca53
John Cho <johnycho16@gmail.com>
2019-03-02 23:00:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,8 +11,7 @@
 
 def reverse_each_word(string)
   array = string.split
-  array.collect {|x| x.reverse }
-  array.join
+  array.collect {|x| x.reverse }.join
 end
 
 # reverse_each_word("Hello there, and how are you?")
